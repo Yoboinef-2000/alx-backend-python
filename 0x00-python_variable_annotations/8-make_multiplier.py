00dcffa691d2198d3666f6fe27cdc39dcf9ec842
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-08-03 10:53:06 -0400

    I pushed more peeter, more sweeter and more completer than any other peter pusher arouond.

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
 """Type annotated make_multiplier function."""
+from typing import Callable
 
 
-def make_multiplier (multiplier:float)
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Multiply'em up."""
+    def laFunctioneMultiplier(x: float) -> float:
+        return x * multiplier
+    
+    return laFunctioneMultiplier
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 """Type annotateed element_length function."""
-from typing import List, Tuple
+from typing import List, Tuple, Sequence, Iterable
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Get that length, and possibly girth."""
     return [(i, len(i)) for i in lst]
