a75a7cc17b9fb7861e6a6a0b42a61e3ce5ef16dd
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-08-03 11:27:11 -0400

    I pushed more peeter, more sweeter and more completer than any other peter pusher arouond.

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""Using Mypy to validate the code below."""
+from typing import List, Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Mypy'em up."""
+    zoomed_in: List[int] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
