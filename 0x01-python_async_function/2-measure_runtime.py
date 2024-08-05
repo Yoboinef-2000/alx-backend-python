927e89dc1196d0422e69ef292ccfa1d7e52cb4af
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-08-05 18:05:30 -0400

    All in pink.

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,11 +7,11 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time (n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Measure the time wait_n takes."""
     startTimerBaby = time.time()
     asyncio.run(wait_n(n, max_delay))
     endTimer_WellKinda = time.time()
 
-    olympicTime = endTimer_WellKinda - startTimerBaby 
+    olympicTime = endTimer_WellKinda - startTimerBaby
     return olympicTime / n
