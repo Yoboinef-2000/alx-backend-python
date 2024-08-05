29c895b885b257f7d123719b0613638cd9bf2fa4
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-08-05 17:44:02 -0400

    All in pink.

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,6 +6,7 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Async coroutines."""
     delayList: List[float] = []
@@ -13,4 +14,3 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
         delulu = await wait_random(max_delay)
         delayList.append(delulu)
     return sorted(delayList)
-    
