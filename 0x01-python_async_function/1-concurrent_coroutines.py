72bb6f53bd13d5341a15b3e8ee450ad0199db9e4
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-08-05 17:27:07 -0400

    All in pink.

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,13 +3,21 @@
 """More async, await."""
 import asyncio
 from typing import List
+from heapq import heappush, heappop
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n, max_delay) -> List[float]:
     """Async coroutines."""
-    delayList: List[float] = []
+    # delayList: List[float] = []
+    # for _ in range(n):
+    #     delulu = await wait_random(max_delay)
+    #     delayList.append(delulu)
+    # return sorted(delayList)
+    heap: List[float] = []
     for _ in range(n):
         delulu = await wait_random(max_delay)
-        delayList.append(delulu)
-    return sorted(delayList)
+        heappush(heap, delulu)
+
+    delayList = [heappop(heap) for _ in range(n)]
+    return delayList
