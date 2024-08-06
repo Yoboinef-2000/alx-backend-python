f5f556fcceace2571524d1b30046f1367dcc6aee
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-08-06 14:26:27 -0400

    First Day In - Contrary to Tee Grizzley's First day out.

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,9 +2,19 @@
 
 """Implementing asyncio gather."""
 import asyncio
+import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime():
-    """"""
-    
+    """Measure the run time."""
+    startTimerBaby = time.time()
+
+    coroutines = []
+    for _ in range(4):
+        coroutines.append(async_comprehension())
+    await asyncio.gather(*coroutines)
+
+    endTimer_WellKinda = time.time()
+    olympicTime = endTimer_WellKinda - startTimerBaby
+    return olympicTime
