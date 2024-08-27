0c3b1f1062c43582c224248737dca8e8778b884c
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-08-26 22:45:14 -0400

    Testing has_license.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -38,3 +38,15 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient("test_org")
         self.assertEqual(client._public_repos_url,
                          "https://api.github.com/orgs/test_org/repos")
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected_result):
+        """Testing if has_license returns the correct result."""
+
+        client = GithubOrgClient("test_org")
+        with patch.object(client, 'repo', repo):
+            result = client.has_license(license_key)
+            self.assertEqual(result, expected_result)
